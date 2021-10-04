8efe8c30921c6229b6bda0165659682eb57a257a
brnsalg <salgado.bhn@gmail.com>
2021-10-04 19:05:57 -0300

    n vai pra segunda tela

--- app/src/main/java/com/example/trabalho2/MainActivity.java
+++ app/src/main/java/com/example/trabalho2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.trabalho2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,23 +10,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText p1;
-    private EditText p2;
-    private EditText p3;
-    private TextView resultado;
+    private EditText p1, p2, p3;
+    private TextView resultado, status;
     private Boolean media;
-    private Button calcular;
+    private Button calcular, proximaTela;
+    public static final String EXTRA_TEXT1 = "com.example.application.example.EXTRA_TEXT1";
+    public static final String EXTRA_TEXT2 = "com.example.application.example.EXTRA_TEXT2";
+    public static final String EXTRA_TEXT3 = "com.example.application.example.EXTRA_TEXT3";
+    public static final String EXTRA_TEXT4 = "com.example.application.example.EXTRA_TEXT4";
+    public static final String EXTRA_TEXT5 = "com.example.application.example.EXTRA_TEXT5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultado = findViewById(R.id.resultado);
+        resultado = findViewById(R.id.resultado_final);
         p1 = findViewById(R.id.p1);
         p2 = findViewById(R.id.p2);
         p3 = findViewById(R.id.p3);
         calcular = findViewById(R.id.calcular);
+        status = findViewById(R.id.status);
+
+        proximaTela = findViewById(R.id.proxTela);
+        proximaTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOutraTela();
+            }
+        });
     }
 
     public void calcular (View view) {
@@ -44,9 +57,27 @@ public class MainActivity extends AppCompatActivity {
 
         if (media >= 6) {
             resultado.setText("Aprovado! Sua média foi " + media);
+            status.setText("Aprovado");
         } else {
             resultado.setText("Reprovado! Sua média foi " + media);
+            status.setText("Reprovado");
         }
+    }
+
+    public void openOutraTela() {
+        double nota1 = Double.parseDouble(p1.getText().toString());
+        double nota2 = Double.parseDouble(p2.getText().toString());
+        double nota3 = Double.parseDouble(p3.getText().toString());
+        double mediaString = Double.parseDouble(media.toString());
+        String status_final = status.getText().toString();
+
+        Intent intent = new Intent(this, outra_tela.class);
+        intent.putExtra(EXTRA_TEXT1, nota1);
+        intent.putExtra(EXTRA_TEXT2, nota2);
+        intent.putExtra(EXTRA_TEXT3, nota3);
+        intent.putExtra(EXTRA_TEXT4, mediaString);
+        intent.putExtra(EXTRA_TEXT5, status_final);
+        startActivity(intent);
     }
 
     public void limpar (View view) {
@@ -54,5 +85,6 @@ public class MainActivity extends AppCompatActivity {
         p2.setText("");
         p3.setText("");
         resultado.setText("");
+        status.setText("");
     }
 }
---
+++ app/src/main/java/com/example/trabalho2/outra_tela.java
@@ -0,0 +1,87 @@
+package com.example.trabalho2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class outra_tela extends AppCompatActivity {
+
+    private Button anteriorTela;
+    private TextView resultado_final, status, faltas;
+    private EditText p1, p2, p3, nome, disciplina, media;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outra_tela);
+
+        Intent intent = getIntent();
+        String nota_11 = intent.getStringExtra(MainActivity.EXTRA_TEXT1);
+        String nota_22 = intent.getStringExtra(MainActivity.EXTRA_TEXT2);
+        String nota_33 = intent.getStringExtra(MainActivity.EXTRA_TEXT3);
+        String media_media_media = intent.getStringExtra(MainActivity.EXTRA_TEXT4);
+        String status_status_status = intent.getStringExtra(MainActivity.EXTRA_TEXT5);
+
+        p1 = findViewById(R.id.nota_p1);
+        p1.setText(nota_11);
+
+        p2 = findViewById(R.id.nota_p2);
+        p2.setText(nota_22);
+
+        p3 = findViewById(R.id.nota_p3);
+        p3.setText(nota_33);
+
+        media = findViewById(R.id.nota_media);
+        media.setText(media_media_media);
+
+        status = findViewById(R.id.status_final);
+        status.setText(status_status_status);
+
+        disciplina = findViewById(R.id.disciplina);
+
+        faltas = findViewById(R.id.faltas);
+
+        anteriorTela = findViewById(R.id.voltar);
+        anteriorTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void finalizar (View view) {
+        String nomePessoa = String.valueOf(nome.getText());
+        String status_f = String.valueOf(status.getText());
+        String disci = String.valueOf(disciplina.getText());
+        String p1_final = String.valueOf(p1.getText());
+        String p2_final = String.valueOf(p2.getText());
+        String p3_final = String.valueOf(p3.getText());
+        String media_final = String.valueOf(media.getText());
+        String faltas_final = String.valueOf(faltas.getText());
+        if (status_f == "Aprovado") {
+            resultado_final.setText("O(A) aluno(a)" + nomePessoa + " foi " + status_f + " com a média de " + media_final + " em " + disci + " com as notas: P1=" + p1_final + ", P2=" + p2_final + ", P3=" + p3_final + " e " + faltas_final + " faltas!");
+        } else {
+            resultado_final.setText("O(A) aluno(a)" + nomePessoa + " foi " + status_f + " com a média de " + media_final + " em " + disci + " com as notas: P1=" + p1_final + ", P2=" + p2_final + ", P3=" + p3_final + " e " + faltas_final + " faltas!");
+        }
+    }
+
+    public void compartilhar (View view) {
+        String result = resultado_final.getText().toString();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(sendIntent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        sendIntent.putExtra(sendIntent.EXTRA_TEXT, "" + result);
+        startActivity(sendIntent);
+    }
+}
